71ce448422813f0161c0215252757d0d8aab1e31
Peter Liu <onhout@gmail.com>
2017-08-21 16:35:26 -0700

    console logging target vs original

--- market_trade_v3.js
+++ market_trade_v3.js
@@ -244,9 +244,13 @@ class Tradev3 {
                                 } else {
                                     console.log('Not enough quantity, forget it.');
                                     console.log('**Want to buy(BTC): ' + BUYFROM.buyOptions.quantity + '| have: ' + buy_order_book.result.buy[0].Quantity + '**');
+                                    console.log('**Want to target buy(BTC): ' + BUYFROM.buyOptions.quantity + '**');
                                     console.log('**Want to sell(BTC): ' + BUYFROM.buyOptions.quantity + '| have: ' + buy_order_book.result.sell[0].Quantity + '**');
+                                    console.log('**Want to target sell(BTC): ' + BUYFROM.buyOptions.quantity * 3 + '**');
                                     console.log('**Want to buy(ETH): ' + BuyEthQuantity + '| have: ' + sell_order_book.result.buy[0].Quantity + '**');
+                                    console.log('**Want to target buy(ETH): ' + BuyEthQuantity * 3 + '**');
                                     console.log('**Want to sell(ETH): ' + BuyEthQuantity + '| have: ' + sell_order_book.result.sell[0].Quantity + '**');
+                                    console.log('**Want to target sell(ETH): ' + BuyEthQuantity * 3 + '**');
                                 }
                             });
                         });
